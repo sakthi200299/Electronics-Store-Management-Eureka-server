e2a032d2bc3d81158a6d8e297f7ed9ba7cdca557
sakthi200299 <sv212766@gmail.com>
2024-07-12 11:38:55 +0530

    new Eureka-Server MicroService is added

---
+++ servicediscovery/src/test/java/com/springboot/gateway/ServicediscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicediscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
